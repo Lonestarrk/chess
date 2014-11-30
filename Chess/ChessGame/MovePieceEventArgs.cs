a7316bed739ea503d9108438d95253521295a3a3
Jimmy <revtorparen@gmail.com>
2014-11-30 01:51:02 +0100

    crated events to handle some of the game logic. selection and movement

---
+++ Chess/ChessGame/MovePieceEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessGame
+{
+    public class MovePieceEventArgs : EventArgs
+    {
+        public Position FromPosition { get; set; }
+        public Position ToPosition { get; set; }
+        public MovePieceEventArgs(Position fromPosition, Position toPosition)
+        {
+            FromPosition = fromPosition;
+            ToPosition = toPosition;
+        }
+      
+    }
+}
---
+++ Chess/ChessGame/SelectPieceEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessGame
+{
+    public class SelectPieceEventArgs : EventArgs
+    {
+        public Square Square { get; set; }
+        public SelectPieceEventArgs(Square square)
+        {
+            Square = square;
+        }
+    }
+}
